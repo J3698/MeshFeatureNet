ba290169fd391f8c8626cfb88363ce4cc56a6772
Ubuntu <ubuntu@ip-172-31-69-107.ec2.internal>
2020-06-26 22:59:44 +0000

    Remove softras download util, fix download names

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1JRE3fzIS9q2Jvou-DZuogY9VHiTftdb_' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1JRE3fzIS9q2Jvou-DZuogY9VHiTftdb_" -O data/MN40obj.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1JRE3fzIS9q2Jvou-DZuogY9VHiTftdb_' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1JRE3fzIS9q2Jvou-DZuogY9VHiTftdb_" -O data/MN40Objs.zip && rm -rf /tmp/cookies.txt
 
 echo "Silently exctracting zip..."
-unzip data/MN40obj > /dev/null
+unzip data/MN40Objs > /dev/null
+mv MN40Objs data/MN40Objs
 echo "Done downloading and extracting"
