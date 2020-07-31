62bd525a72df308694a141fcf875b52c5bc1915c
Antioch Sanders <antiochsanders@gmail.com>
2020-07-31 11:37:38 -0400

    Add missing import

--- test.py
+++ test.py
@@ -1,116 +1,91 @@
 import argparse
 
 import torch
-import torch.nn.parallel
-import datasets
-from utils import AverageMeter, img_cvt
+from sklearn import svm
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import numpy as np
+from losses import multiview_iou_loss
+from utils import AverageMeter, imgs_to_gif
 import soft_renderer as sr
 import soft_renderer.functional as srf
+from tqdm import tqdm
+import datasets
 import models
-import time
-import os
 import imageio
-import numpy as np
+import time
+from datetime import datetime
+import os
+from ground_truth_rendering import GroundTruthRenderer
 
-BATCH_SIZE = 100
+
+resume_path = "data/results/models/2020-07-30-09:02:57.021496/" + \
+              "checkpoint_0005300.pth.tar"
+
+SIGMA_VAL = 1e-4
 IMAGE_SIZE = 64
-CLASS_IDS_ALL = (
-    '02691156,02828884,02933112,02958343,03001627,03211117,03636649,' +
-    '03691459,04090263,04256520,04379243,04401088,04530566')
+VIEWS = 12
 
-PRINT_FREQ = 100
-SAVE_FREQ = 100
-
-MODEL_DIRECTORY = './data/models'
-DATASET_DIRECTORY = './data/datasets'
-
-SIGMA_VAL = 0.01
-IMAGE_PATH = ''
-
-# arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-eid', '--experiment-id', type=str)
-parser.add_argument('-d', '--model-directory', type=str, default=MODEL_DIRECTORY)
-parser.add_argument('-dd', '--dataset-directory', type=str, default=DATASET_DIRECTORY)
-
-parser.add_argument('-cls', '--class-ids', type=str, default=CLASS_IDS_ALL)
 parser.add_argument('-is', '--image-size', type=int, default=IMAGE_SIZE)
-parser.add_argument('-bs', '--batch-size', type=int, default=BATCH_SIZE)
-parser.add_argument('-img', '--image-path', type=str, default=IMAGE_PATH)
-
 parser.add_argument('-sv', '--sigma-val', type=float, default=SIGMA_VAL)
-
-parser.add_argument('-pf', '--print-freq', type=int, default=PRINT_FREQ)
-parser.add_argument('-sf', '--save-freq', type=int, default=SAVE_FREQ)
 args = parser.parse_args()
 
-# setup model & optimizer
-model = models.Model('data/obj/sphere/sphere_642.obj', args=args)
+
+start_iter = int(os.path.split(resume_path)[1][11:].split('.')[0]) + 1
+print('Loading model from iteration %s' % start_iter)
+model = models.Model('data/obj/sphere/sphere_1352.obj', args=args)
+model = nn.DataParallel(model)
 model = model.cuda()
-
-state_dicts = torch.load(args.model_directory)
-model.load_state_dict(state_dicts['model'], strict=False)
-model.eval()
-
-dataset_val = datasets.ShapeNet(args.dataset_directory, args.class_ids.split(','), 'val')
-
-directory_output = './data/results/test'
-os.makedirs(directory_output, exist_ok=True)
-directory_mesh = os.path.join(directory_output, args.experiment_id)
-os.makedirs(directory_mesh, exist_ok=True)
+state_dicts = torch.load(resume_path)
+model.load_state_dict(state_dicts['model'])
 
 
-def test():
-    end = time.time()
+dataset_directory = 'data/MN40Objs'
 
-    batch_time = AverageMeter()
-    data_time = AverageMeter()
-    losses = AverageMeter()
-    losses1 = AverageMeter()
+gtr = GroundTruthRenderer(IMAGE_SIZE, SIGMA_VAL, VIEWS)
+dataset_test = datasets.ModelNet40(dataset_directory, partition='test',
+                                   truncate = None)
+test_loader = DataLoader(dataset_test, batch_size = 16, shuffle = False)
+dataset_train = datasets.ModelNet40(dataset_directory, partition='train',
+                                    truncate = None)
+test_loader = DataLoader(dataset_test, batch_size = 16, shuffle = True)
 
-    iou_all = []
+cat_map = dataset_train.categories
 
-    for class_id, class_name in dataset_val.class_ids_pair:
+def get_svm_data(data_loader):
+    print("Getting SVM features")
+    x_data = []
+    y_data = []
+    for i, paths in tqdm(enumerate(data_loader)):
+        data = [gtr.render_ground_truth(path, chunk=12) for path in paths]
+        images, viewpoints = zip(*data)
+        images = torch.cat([k.unsqueeze(0) for k in images], axis = 0)
+        viewpoints = torch.cat([v.unsqueeze(0) for v in viewpoints], axis = 0)
+        categories = [dataset_train.get_category(i) for i in paths]
+        batch_size = len(images)
+        assert batch_size == len(categories) and batch_size == len(viewpoints)
 
-        directory_mesh_cls = os.path.join(directory_mesh, class_id)
-        os.makedirs(directory_mesh_cls, exist_ok=True)
-        iou = 0
-        
-        for i, (im, vx) in enumerate(dataset_val.get_all_batches_for_evaluation(args.batch_size, class_id)):
-            images = torch.autograd.Variable(im).cuda()
-            voxels = vx.numpy()
+        feats = model.module.compute_features(images).detach().cpu()
+        feats = [i for i in feats]
+        assert all(len(feat) == model.module.encoder.dim_out for feat in feats)
 
-            batch_iou, vertices, faces = model(images, voxels=voxels, task='test')
-            iou += batch_iou.sum()
-            
-            batch_time.update(time.time() - end)
-            end = time.time()
+        answers = [cat_map[j] for j in categories]
+        x_data += feats
+        y_data += answers
 
-            # save demo images
-            for k in range(vertices.size(0)):
-                obj_id = (i * args.batch_size + k)
-                if obj_id % args.save_freq == 0:
-                    mesh_path = os.path.join(directory_mesh_cls, '%06d.obj' % obj_id)
-                    input_path = os.path.join(directory_mesh_cls, '%06d.png' % obj_id)
-                    srf.save_obj(mesh_path, vertices[k], faces[k])
-                    imageio.imsave(input_path, img_cvt(images[k]))
-
-            # print loss
-            if i % args.print_freq == 0:
-                print('Iter: [{0}/{1}]\t'
-                      'Time {batch_time.val:.3f}\t'
-                      'IoU {2:.3f}\t'.format(i, ((dataset_val.num_data[class_id] * 24) // args.batch_size), 
-                                             batch_iou.mean(),
-                                             batch_time=batch_time))
-
-        iou_cls = iou / 24. / dataset_val.num_data[class_id] * 100
-        iou_all.append(iou_cls)
-        print('=================================')
-        print('Mean IoU: %.3f for class %s' % (iou_cls, class_name))
-        print('\n')
-
-    print('=================================')
-    print('Mean IoU: %.3f for all classes' % (sum(iou_all) / len(iou_all)))
+    return x_data, y_data
 
 
-test()
+def test_accuracy():
+    # svm_train_feats, svm_train_labels = get_svm_data(train_loader)
+    svm_test_feats, svm_test_labels = get_svm_data(test_loader)
+    classifier = svm.LinearSVC()
+    print("Fitting SVM")
+    classifier.fit(svm_test_feats, svm_test_labels)
+
+    predicted_labels = classifier.predict(svm_test_feats)
+    acc = sum(svm_test_labels == predicted_labels) / len(predicted_labels)
+    return acc
+
+print(test_accuracy())
