ad1c53660f5875617ac3b9db695d24f5993a0948
Antioch Sanders <antiochsanders@gmail.com>
2020-08-06 01:54:54 -0400

    Compare accuracies accross 642 v 1352 vertices

--- test.py
+++ test.py
@@ -21,8 +21,8 @@ import sys
 from ground_truth_rendering import GroundTruthRenderer
 
 
-model1 = "/data/datasets/asanders/MeshFeatureNet/data/results/models/2020-08-03-13:44:28.549495"
-model2 = "/data/datasets/asanders/MeshFeatureNet/data/results/models/2020-08-03-13:42:10.827513"
+model1_path = "/data/datasets/asanders/MeshFeatureNet/data/results/models/2020-08-03-13:44:28.549495"
+model2_path = "/data/datasets/asanders/MeshFeatureNet/data/results/models/2020-08-03-13:42:10.827513"
 
 dataset_directory = 'data/MN40Objs'
 
@@ -36,26 +36,37 @@ parser.add_argument('-sv', '--sigma-val', type=float, default=SIGMA_VAL)
 args = parser.parse_args()
 
 gtr = GroundTruthRenderer(IMAGE_SIZE, SIGMA_VAL, VIEWS)
+
 dataset_test = datasets.ModelNet40(dataset_directory, partition='test',
                                    truncate = None)
 test_loader = DataLoader(dataset_test, batch_size = 16, shuffle = False)
+
 dataset_train = datasets.ModelNet40(dataset_directory, partition='train',
                                     truncate = None)
-test_loader = DataLoader(dataset_test, batch_size = 16, shuffle = True)
+train_loader = DataLoader(dataset_train, batch_size = 16, shuffle = False)
+
 cat_map = dataset_train.categories
 
 
 
 def main():
-    model = models.Model('data/obj/sphere/sphere_1352.obj', args=args)
-    model = nn.DataParallel(model)
-    model = model.cuda()
-    for model in zip(get_checkpoints(model, model1), get_checkpoints(model, model2)):
-        test_accuracy(model)
+    model1 = models.Model('data/obj/sphere/sphere_1352.obj', args=args)
+    model1 = nn.DataParallel(model1)
+    model1 = model1.cuda()
+
+    model2 = models.Model('data/obj/sphere/sphere_642.obj', args=args)
+    model2 = nn.DataParallel(model2)
+    model2 = model2.cuda()
+
+    to_compare = zip(get_checkpoints(model1, model1_path), get_checkpoints(model2, model2_path))
+    for modela, modelb in to_compare:
+        print("1", test_accuracy(modela))
+        print("2", test_accuracy(modelb))
 
 
 def get_checkpoints(model, model_path):
-    for checkpoint in os.listdir(model_path):
+    paths = sorted(os.listdir(model_path))
+    for checkpoint in paths:
         if checkpoint.endswith("pth.tar"):
             resume_path = os.path.join(model_path, checkpoint)
             print(resume_path)
@@ -77,8 +88,8 @@ def get_svm_data(model, data_loader):
         batch_size = len(images)
         assert batch_size == len(categories) and batch_size == len(viewpoints)
 
-        feats = model.module.compute_features(images).detach().cpu()
-        feats = [i for i in feats]
+        feats = model.module.compute_features(images).detach().cpu().numpy()
+        feats = [i.tolist() for i in feats]
         assert all(len(feat) == model.module.encoder.dim_out for feat in feats)
 
         answers = [cat_map[j] for j in categories]
@@ -89,11 +100,11 @@ def get_svm_data(model, data_loader):
 
 
 def test_accuracy(model):
-    # svm_train_feats, svm_train_labels = get_svm_data(model, train_loader)
+    svm_train_feats, svm_train_labels = get_svm_data(model, train_loader)
     svm_test_feats, svm_test_labels = get_svm_data(model, test_loader)
     classifier = svm.LinearSVC()
     print("Fitting SVM")
-    classifier.fit(svm_test_feats, svm_test_labels)
+    classifier.fit(svm_train_feats, svm_train_labels)
 
     predicted_labels = classifier.predict(svm_test_feats)
     acc = sum(svm_test_labels == predicted_labels) / len(predicted_labels)
